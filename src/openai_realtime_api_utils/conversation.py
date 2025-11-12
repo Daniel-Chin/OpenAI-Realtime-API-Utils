14373716085757a84b1db6ef8442f662209a8d49
Daniel Chin <daniel.chin@nyu.edu>
2025-11-12 12:15:28 +0400

    conversation

--- src/openai_realtime_api_utils/conversation.py
+++ src/openai_realtime_api_utils/conversation.py
@@ -2,65 +2,45 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from .shared import *
-
 class Conversation:
     @dataclass
     class Cell:
-        parent: Conversation
-        prev: ItemID | None
-        next_: ItemID | None
-
+        item_id: str
         # (content_index, audio_end_ms)
-        audio_truncate: tp.Tuple[int, int] | None = None
-        modified_by: tp.List[EventID] = []
-    
-        def nextCell(self):
-            if self.next_ is None:
-                raise StopIteration
-            return self.parent.cells[self.next_]
+        audio_truncate: tuple[int, int] | None = None
+        touched_by_events: list[str] = []
 
-    def __init__(self):
-        self.cells: tp.Dict[ItemID, Conversation.Cell] = {}
-        self.root: ItemID | None = None
+    def __init__(self) -> None:
+        self.cells: list[Conversation.Cell] = []
+    
+    def seek(self, item_id: str) -> int:
+        cell_i, = [
+            i for i, cell in enumerate(self.cells) 
+            if cell.item_id == item_id
+        ]
+        return cell_i
     
     def insertAfter(
-        self, item_id: ItemID, 
-        previous_item_id: ItemID | None, 
-    ):
-        if previous_item_id is None:
-            assert self.root is None
-            self.root = item_id
-            cell = self.Cell(self, None, None)
-            self.cells[item_id] = cell
-            return cell
-        prevCell = self.cells[previous_item_id]
-        next_id = prevCell.next_
-        cell = self.Cell(self, previous_item_id, next_id)
-        self.cells[item_id] = cell
-        prevCell.next_ = item_id
-        if next_id is not None:
-            self.cells[next_id].prev = item_id
-        return cell
+        self, item_id: str, 
+        previous_item_id: str | None, 
+    ) -> Cell:
+        new_cell = self.Cell(item_id)
+        self.cells.insert((
+            0 if previous_item_id is None 
+            else self.seek(previous_item_id) + 1
+        ), new_cell)
+        return new_cell
     
-    def pop(self, item_id: ItemID, /):
-        cell = self.cells.pop(item_id)
-        prev_id = cell.prev
-        next_id = cell.next_
-        if prev_id is not None:
-            self.cells[prev_id].next_ = next_id
-        if next_id is not None:
-            self.cells[next_id].prev = prev_id
-        if item_id == self.root:
-            self.root = next_id
+    def previousItemIdOf(self, item_id: str) -> str | None:
+        cell_i = self.seek(item_id)
+        if cell_i == 0:
+            return None
+        else:
+            return self.cells[cell_i - 1].item_id
     
-    def __iter__(self):
-        item_id = self.root
-        while item_id is not None:
-            cell = self.cells[item_id]
-            yield item_id, cell
-            item_id = cell.next_
+    def pop(self, item_id: str) -> Cell:
+        return self.cells.pop(self.seek(item_id))
     
-    def touched(self, item_id: ItemID, event_id: EventID):
-        cell = self.cells[item_id]
-        cell.modified_by.append(event_id)
+    def touch(self, item_id: str, event_id: str) -> None:
+        cell = self.cells[self.seek(item_id)]
+        cell.touched_by_events.append(event_id)
